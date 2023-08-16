1f733d1cd02e06ec9c4b0c804a4b4a88cc67e4e2
Tomas Storc <tomas.storc@gmail.com>
2023-08-16 13:37:46 +0200

    first version done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,268 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Recenze Unicorm University po dvou letech studia</h1>
+      <h2>Úvod</h2>
+      <p>
+        Pokusím se popsat své dojmy a zkušenosti ze dvou let studia na Unicorn
+        University. Možná to někomu pomůže získat pohled na školu i z jiného
+        úhlu než jen z promo materiálů, kterých tato škola produkuje mnoho. V IT
+        pracuji již několik let, a už před zahájením studia jsem měl pár let
+        zkušností v oboru, takže se na školu snažím nahlížet s větším nadhledem.
+        Studuji obor softwarový vývoj v online formě.
+      </p>
+      <p>
+        Tuto recenzi bych rád rozdělil do třech hlavních části. V první se
+        podiváme na samotné studium, co Vám přinese, pakliže se pro školu
+        rozhodnote, kvalitu výuky a vyučujících a jak je výuka zaměřena. V druhé
+        části bych rád popsal, jak je to s organizací studia a ostatními
+        administrativními záležitosmi, s kterými se chtě nechtě na škole
+        potkáte. V poslední části si vše shrneme. Pojďme na to.
+      </p>
+      <h2>Studium</h2>
+      <h3>Co se zde naučíte</h3>
+      <p>
+        Školu jdete studovat hlavně proto, abyste se něco naučili. Vysoká škola,
+        by Vám měla dát odpovědi na otázky typu proč, abyste svému oboru
+        rozuměli. To je jeden z důvodu proč na "tradičních" vysokých školách
+        vyučují programovací jazyky C/C++. Není to proto, že by tyto školy
+        zamrzly v minulém století ale proto, že právě tyto jazyky dobře ukazují
+        základy programování. Na Unicorn University nic takového nečekejte.
+      </p>
+      <p>
+        V prvním ročníku začnete s čistým JavaScriptem, následně už Vás škola
+        pomalu ale jistě začne připravovat na jejich framework, postaven nad
+        JavaScriptem, respektive Reactem na frontendu a Node.js na backendu.
+        Toto je dle mne největší kámen úrazu. Škola se chlubí prakticky
+        orientovanou výukou, což je pravda, ale pokud se budete držet
+        technologii, které škola vyučuje, tak po vystudování bakalářského oboru
+        softwarový vývoj budete umět programovat ve frameworku uuApp od
+        společnosti Unicorn. S přihlédnutím na to, že škola není zdarma je to
+        dle mého názoru trochu málo.
+      </p>
+      <p>
+        Vyučující sice tvrdí, že je to jedno, zda se učíte v uuApp frameworku,
+        nebo v něčem, co opravdu funguje a je známo po celém světě, ale rozdíl v
+        tom bohužel je, takže pokud budete studovat čistě podle osnovy, tak
+        budete maximálně tak připraveni na to, jít pracovat pro Unicorn.
+      </p>
+      <p>
+        Škola vyučuje i jiné odborné předměty, jako je algoritmizace, pokročilá
+        algoritmizace nebo design patterns. Tyto předměty Vám dají dobrý přehled
+        o oboru obecně. Otázka ovšem je, zda toto není povinnost, aby škola
+        mohla vůbec existovat. Jinak by se tu pravděpodobně vyučoval pouze uuApp
+        a ostatní uu věci, a po třech letech by z Vás byl uuBakalář.
+      </p>
+      <p>
+        Poslední předmět, který stojí za zmínku je angličtina. Ta je ve všech
+        semestrech a v každém semestru budete dělat naprosto to samé. Uděláte
+        uuKurz, kouknete na uuOpen, protože za to jsou body z testu (zřejmě
+        snaha o nahnání sledujících), napíšete test, a odprezentujete
+        prezentaci, naštěstí vyučující si nepamatují, co jste prezentovali
+        minulý semstr, takže není potřeba měnit téma.
+      </p>
+      <h3>Kdo Vás to naučí</h3>
+      <p>
+        Zde bych se rád zaměřil především na odborné předměnty. Jak správně
+        tušíte, tak kdo jiný by mohl učit framework, který používá výhradně
+        firma Unicorn než lidé z Unicornu? Nerad bych tu rozebíral kvalitu firmy
+        Unicorn a jejich zaměstanců, věřím, že se jedná o odborníky na vývoj
+        softwaru. Ovšem, to že je někdo dobrý ve svém oboru automaticky
+        neznamená, že je dobrý vyučující, Jsou zde i vyjímky, a tito lidé jsou
+        opravdu schopni Vám danou problematiku dobře vysvětlit, ale jinak
+        kvalita výuky nestojí za moc.
+      </p>
+      <p>
+        Uvedu dva příklady. Během svého studia jsem si zapsal předmět "UX při
+        vývoji produktu". Tento předmět je vyučován i synem zakladatele Unicornu
+        Vladimíra Kováře. Během výuky nám bylo řečeno, že pánové vyučující by
+        měli o víkendu lepší věci na práci než nás učit a neustále se nad nás
+        povyšovali, že my tomu nerozumíme a oni ano. To dává smysl, že nejsme
+        tak dobří, jako oni, proto jsme si daný předmět zapsali. O kvalitě
+        těchto lidí by se také mohlo pochybovat, jelikož údajně měli vlastní
+        firmu, která nabízela grafické služby a nakonec skončili v Unicornu.
+        Tento přístup vyučujících ve Vás zvědavost po UX nevzbudí, spíš naopak.
+      </p>
+      <p>
+        Druhým příkladem je předmět "Backendové systémy", kde se samozřejmě
+        vyučuje uuApp. Osobně jsem se rozhodl pro framework Nest.js, který je
+        rozhodně světově rozšířenější, než právě firmou Unicorn oblíbený uuApp.
+        Při hodnocení mi vyučující vytkl, chybějící implentaci autentizace a
+        autorizace, i přes to, že implentována byla. Tento vyučující naštěstí
+        již na škole nepůsobí, pravděpodobně i kvůli jeho arogantnímu přístupu
+        ke studentům na hodinách.
+      </p>
+      <h3>Workshopy kam se podíváte</h3>
+      <p>
+        V každém semestru Vás čeká prakticky orientovaný workshop. Jedná se o
+        snahu několika lidí, kteří se většinou neznají vytvořit aplikaci během
+        semestru a nakonec během víkendu, kdy se aplikace dodělává a následně
+        prezentuje. Co se týče samotného zadání, tak se nejedná o nic složitého.
+        Jednoduchá CRUD aplikace Vás čeká celkem ve třech semstrech.
+      </p>
+      <p>
+        Aplikace potřebuje dokumentaci, která se dělá, někteří už možná tušíte,
+        v Unicorním nástroji na tvorbu dokumentace. Práce se musí organizovat,
+        vytvářet tasky, sprinty a tak dále. V čem se toto dělá už jistě tušíte,
+        ano, opět Unicorní nástroj. Dle mého názoru by bylo mnohem pro studenty
+        přínosnější kdyby jim firma poskytla industry standard nástroje, jako je
+        Confluence a Jira.
+      </p>
+      <h3>Co se zde naučíte kromě programování</h3>{" "}
+      <p>
+        V krátkosti spíš nic. Zatímco na většině škol, budete mít alespoň
+        základy operačních systémů a sítě, tak tady se dočkáte maximálně
+        internetu věcí. Ačkoliv se možná jedná o aktuální téma, tak by bylo
+        možná lepší studenty naučit pracovat právě s příkazovou řádkou, která je
+        při vývoji softwaru opravdu potřebná.
+      </p>
+      <p>
+        Dobrá zpráva je, že se zde naučíte nasazovat aplikace do cloudu, ta
+        špatná zpráva je, že do uuCloudu, pokud jste o uuCloudu do teď
+        neslyšeli, tak se není čemu divit. Kromě Unicornu, ho nikdo jiný
+        nepoužívá, naštěstí. Takže se jedná o další věc, kterou se zde sice
+        naučíte, ale jako víceméně vše, i toto Vám bude k ničemu.
+      </p>
+      <h3>Studijní materiály</h3>
+      <p>
+        Škola na internetu zmiňuje, že její studijní materály jsou dostupné
+        online, a že se tedy můžete vzdělávat odkudkoliv, pokud teda zrovna
+        fungují stránky školy. Ano můžete, jen to prosím nedělejte. Některé
+        studijní materiály, jsou zpracované dostatečně, ovšem o některých se to
+        říci nedá, a je lepší čerpat i z jiných zdrojů. V případě, že tu
+        přetrpíte až ke státnicím, tak Vám stejně nic jiného nezbyde.
+      </p>
+      <p>
+        U některých předmětů Vás čekají i kurzy, za které získáváte body. V
+        některých předmětech jde o zanedbatelné množství bodů, u některých to
+        ovšem tvoří až polovinu bodů z daného přeemětu. Jak dané kurzy vypadají,
+        když mají v některých případech takovou váhou? Jako vtip, testy v těchto
+        kurzech můžete spouštět několikrát a správné odpovědi máte u otázek v
+        lekcích, z kterých se test skládá, takže to zvládne i cvičená opice na
+        plný počet bodů.
+      </p>
+      <h2>Organizace studia</h2>
+      <h3>Studijní oddělení</h3>
+      <p>
+        Pokud čekáte, že studijní oddělení na soukromé škole funguje dobře, tak
+        to tak není. Ono vlastně většinu času spíš nefunguje, nebo funguje
+        špatně. I tak jednoduchou věc, jako je potvrzení o studiu, jsou schopni
+        Vám vystavit špatně a ne jednou ale třikrát, no neberte to za ty peníze.
+      </p>
+      <p>
+        Nejhorší je, pokud máte požadavek, na který porstě odpovědět nechtěji,
+        tak Vám prostě neodpoví. Momentálně řeším téma své bakalářské práce,
+        bohužel od května jsem ještě neobdržel odpověď.
+      </p>
+      <h3>Zkoušky</h3>
+      <p>
+        Pokud se rozhodnete studovat v online formě, tak do školy nechodíte a
+        všechny zkoušky máte online formou. Pokud čekáte nějaké přísné
+        dozorování na samotných zkouškách, když za to po třech letech utrpení
+        získáte titul, tak to, většinu času, nečekejte. Porovnání mám například
+        s certifikacemi od Microsoftu, kde si Vás hlídají, abyste nepodváděli,
+        mnohem lépe.{" "}
+      </p>
+      <p>
+        Co se týče zkouškových termínů, tak zdarma máte nárok na dva termíny,
+        praxe je ovšem taková, že většina předmětů mám vypsán jeden termín a tím
+        to končí. V případě, že Vám termín nevyhovuje, tak si pravděpodobně
+        budete muset zaplatit individuální.
+      </p>
+      <h3>Mimoškolní aktivity</h3>
+      <p>
+        Když jsem na školu nastupoval, tak jeden z dotazů byl, zda škola nabízí
+        adaptační kurz. Odpověď byla, že ne, že si ho studenti mají zorganizovat
+        sami, škola nic takového neoraganizuje. Letos ovšem škola plánuje poprvé
+        adaptační kurz zorganizovat, gratuluji, aspoň něco. Cena je sice
+        přehnaná, kvalita ubytování nevalná, ale i tak, gratuluji. Otázkou je,
+        co školu k tomuto rozhodnutí dovedlo, předpokládám, že možnost vytřískat
+        ze studentů více peněz. To je z mimoškolních aktivit vše, žádné
+        studentské spolky, žádné akce kromě těch pro firmu Unicorn, prostě nic.
+      </p>
+      <h3>Informační systém</h3>
+      <p>
+        Těžko říct, zda to co se zde nazývá informačním systémem opravdu
+        informační systém je. Pokud rádi hledáte informace schované na růných
+        stránkách místo toho, aby vše bylo na jednom místě, tak bude tento
+        informační systém milovat. Jedná se totiž opravdu pouze o různé
+        komponenty, které se objeví na růzých stránkách, když jste jako student
+        přihlášeni. Osobně si myslím, že by bylo mnohem lepší mít všechno na
+        jednom místě.
+      </p>
+      <h2>Závěr</h2>
+      <h3>Poměr cena/kvalita</h3>
+      <p>
+        Za cenu přesahující 130 000 za tři roky tedy získate zkušensoti s
+        Unicorním frameworkem uuApp, které jinde nezískáte. To zda tyto
+        zkušensti získáte díky kvalitě školy, nebo tím, že uuApp nikde jinde
+        neznají nechám na Vás. Dle mého názoru by bylo mnohem lepší vyučovat
+        některý z rozšířených frameworků založených na JavaScriptu, ať se jedná
+        na frontendu o React, či na backendu o Express. Studentům by to přineslo
+        praktické zkušenosti s technologiemi, které se reálně využívá, když už
+        škola vyzdvihuje prakticky orientovanou výuku.
+      </p>
+      <p>
+        Toto je sice skvělé pro firmu Unicorn, která si vychová studenty, kteří
+        se naučí programovat v jejich frameworku a dokonce jim studenti ještě
+        platí. Zda je toto přínosné i pro studenty nechám na Vás. Samozřejmě je
+        možnost vytvářet zadání i ve frameworku, který preferujete vy, otázka
+        ovšem je, zda se vyplatí za to platit.
+      </p>
+      <h3>Unicorm Universe</h3>
+      <p>
+        Možná jste si všimli, že všechno od Unicornu má před jménem dvě pímena,
+        uu. Jedná se o zkratku Unicorn Universe a tato škola by se klidně mohla
+        přejmenovat na uuUniversity. Jen si to vemte, programuje v uuAppu
+        backend, frontend v uu5, svůj zdrojový kód si verzujete v uuCodebase,
+        potom si ho nasadíte do uuCloudu, práci si organizuje v uuSprintManovi,
+        dokumentaci si píšete v uuDocKitu. Je to prostě uúžasný, až na tom, že
+        Vám to nic nepřinese. Firma Unicorn rozhodně není v pozici, kdy by se
+        její technologie používali celosvětově, dokonce ani u nás se nejdná o
+        rozšířenou technologie, naštěstí, jinak by byl IT svět mnohem horší
+        místo.
+      </p>
+      <p>
+        Když si to rozebereme, uuApp je oproti známým frameworkům strašně
+        neintuitivní a jediný support poskytuje Unicorn. uuCodebase, kromě
+        verzování zdrojového kódu nenabízí ani CI/CD pipeliny. uuCloud jsem
+        naštěstí neměl možnost vyzkoušet, ale lepší než AWS či Azure to hádám
+        nebude. uuSprintMan a uuDocKit jsou takové přichcíplé nástroje oproti
+        tomu, co nabízí Atlassian s Jirou a Confluence.
+      </p>
+      <h3>Proč jít školu studovat</h3>
+      <p>
+        Milujete firmu Unicorn a líbí se Vám jejich framework a chcete se ho
+        naučit, následně nastoupit do Unicornu a začít dělat svojí uuKariéru.
+        Chcete titul, pokud máte již nějaké zkušenosti s IT, tak věřím, že školu
+        zvládnete vystudovat bez obtíží, uu technologie budete ignorovat co se
+        dá, a aplikace napíšete v technologiich, které znáte, nebo má smysl se
+        je naučit.
+      </p>
+      <h3>Co když se ale chcete opravdu něco naučit</h3>
+      <p>
+        Běžte na jinou vysokou školu. Státní školy jsou zadarmo, a dají Vám
+        minimálně teoretický základ. Naučit se jakýkoliv jazyk nebo framework
+        dokážete sami.
+      </p>
+      <p>
+        Nemusíte jít studovat vůbec, v dnešní době najdete spoustu tutoriálů,
+        jak na youtube, tak i na různých platformách. Ne všechny jsou sice
+        zadarmo, ale za peníze, které ušetříte za Unicorn University, si můžete
+        dovolit spoustu kurzů a ještě můžete na dovolneou s celou rodinou.
+      </p>
+      <h3>Shrnutí</h3>
+      <p>
+        Osobně tedy školu nedoporučuji. Za částku převyšující 130 000 se naučíte
+        být uuVývojář, budete mít nervy ze studijního oddělení, které Vám buď
+        neodpoví a nebo odpoví špatně. Když už se škola zaměřuje na praktickou
+        výuku, měla by začít vyučovat technologie, které nbudou reálným přínosem
+        pro studenty a ne jen pro firmu Unicorn.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
